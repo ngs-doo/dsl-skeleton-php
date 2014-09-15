faa0f80d8b494d98ff404410e730194c91203975
Viktor Jagusic <viktor.jagusic@gmail.com>
2014-09-15 08:30:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Introduction
+
+Skeleton project including dependencies and installation procedure for DSL platform
+
+## Installation
+
+Use composer:
+
+    composer create-project dsl-platform/dsl-skeleton
---
+++ config/dev.php
@@ -0,0 +1,29 @@
+<?php
+
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\WebProfilerServiceProvider;
+
+\Symfony\Component\Debug\Debug::enable();
+
+require __DIR__.'/prod.php';
+
+$app['debug'] = true;
+
+$app['twig.options'] = array(
+    'cache' => false,
+);
+$client = new \NGS\Client\RestHttp('http://localhost:9012/', 'revenj', 'revenj');
+\NGS\Client\RestHttp::instance($client);
+$app['dsl.client'] = $app->share(function() use ($client) {
+    return $client;
+});
+$app['dsl'] = $app->share(function() { return new \PhpDslAdmin\DomainService(); });
+
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../var/logs/silex_dev.log',
+));
+
+$app->register($p = new WebProfilerServiceProvider(), array(
+    'profiler.cache_dir' => __DIR__.'/../var/cache/profiler',
+));
+$app->mount('/_profiler', $p);
